5cf060f9a494a6a5f0addd89abc3f829b4c41ec0
hai3345 <hai3345@hanmail.net>
2023-03-09 18:30:46 +0900

    port

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,29 +1,27 @@
 
 import './App.css';
-import {BrowserRouter, Link, Routes} from 'react-router-dom' 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Routes, Route} from 'react-router-dom'; 
+
 import 'bootstrap/dist/css/bootstrap.css'
 import MainPage from './MainPage';
-import ApiPage from "./Api/ApiPage"; 
-import Header from "./header"
+import Header from "./Components/header"
 import { Nav } from 'react-bootstrap';
-
+import  Home from "./page/home"
+import Info from "./page/info"
+import Project from "./page/project"
 
 
 
 function App() {
   return (
     <div className='App'>
-      <Header />
-      <BrowserRouter>
-        <Nav />
-        <switch>
+      <Router>
           <Routes>
-            <Route component={Home} path="/Components"/>
-            <Route  component={project} path="/Components"/>
-          </Routes>
-        </switch>
-      </BrowserRouter>
+            <Route element={<Home />} path="/home"/>
+            <Route element={<Info />} path="/info"/>
+            <Route element={<Project />} path="/project"/>
+          </Routes>        
+      </Router>
     </div>
   );
 }
---
+++ portfolio/src/page/info.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Info = () => {
+
+    <div>
+        <h1>INFO</h1>
+    </div>
+}
+
+export default Info;
